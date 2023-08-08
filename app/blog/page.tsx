9d698c29f9ce7749128d6e0a21af14d3d8124b1c
shakhbozbekusmonov <miracle_0319@bk.ru>
2023-08-08 11:43:18 +0500

    fix error

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -8,14 +8,11 @@ interface Post {
 }
 
 async function getData() {
-  const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts",
-    {
-      next: {
-        revalidate: 60,
-      },
-    }
-  );
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    next: {
+      revalidate: 60,
+    },
+  });
 
   if (!response.ok) throw new Error("Unable to fetch posts!");
 
